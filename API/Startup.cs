0486e6d3a54bd4509188cc7e21a0a7a57a804138
UJ <unclejazzmus@gmail.com>
2021-04-01 15:43:57 -0500

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,9 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors( options => {
+                options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -56,9 +59,11 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+       
+            app.UseCors();
 
             app.UseAuthorization();
 
